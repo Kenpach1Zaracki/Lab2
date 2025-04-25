9768f14dd98ac7a2852e8652c8c9b3f124ea5b1d
Давыдов Максим <jabadadaya12@gmail.com>
2025-04-25 10:02:35 +0700

    Update assembler.asm

--- assembler.asm
+++ assembler.asm
@@ -1,100 +1,35 @@
 section .bss
-    buf resb 256      ; Буфер для ввода
+    buf resb 256      ; Буфер для ввода (до 256 символов)
     n resq 1          ; Количество строк
     m resq 1          ; Количество столбцов
-    x resq 1          ; Текущая X координата
-    y resq 1          ; Текущая Y координата
-    minx resq 1
-    maxx resq 1
-    miny resq 1
-    maxy resq 1
+    x resq 1          ; Текущая координата x
+    y resq 1          ; Текущая координата y
+    minx resq 1       ; Минимальная X координата
+    maxx resq 1       ; Максимальная X координата
+    miny resq 1       ; Минимальная Y координата
+    maxy resq 1       ; Максимальная Y координата
 
 section .data
-    msg_yes db "Yes: (", 0
-    msg_no db "No", 10, 0
-    comma db ", ", 0
-    close_br db ")", 10, 0
+    msg_yes db "Yes: (", 0       ; Сообщение при успешном завершении
+    msg_no db "No", 10, 0        ; Сообщение при ошибке
+    comma db ", ", 0             ; Разделитель координат
+    close_br db ")", 10, 0       ; Закрывающая скобка
 
 section .text
-    global _start
+    global _start   ; Точка входа программы
 
-; ========== ПРОЦЕДУРЫ ==========
-skip_spaces:
-.skip:
-    mov al, [rsi]
-    cmp al, ' '
-    je .inc
-    cmp al, 9
-    je .inc
-    ret
-.inc:
-    inc rsi
-    jmp .skip
-
-parse_int:
-    xor rax, rax
-    xor rbx, rbx
-    mov rcx, 10
-.next_digit:
-    mov bl, [rsi]
-    cmp bl, '0'
-    jb .done
-    cmp bl, '9'
-    ja .done
-    sub bl, '0'
-    imul rax, rcx
-    add rax, rbx
-    inc rsi
-    jmp .next_digit
-.done:
-    ret
-
-print_string:
-    mov rdx, 0
-.strlen:
-    cmp byte [rsi + rdx], 0
-    je .print
-    inc rdx
-    jmp .strlen
-.print:
-    mov rax, 1
-    mov rdi, 1
-    syscall
-    ret
-
-print_number:
-    mov rcx, 10
-    sub rsp, 32
-    mov rdi, rsp
-    add rdi, 32
-    mov rbx, 0
-.convert:
-    xor rdx, rdx
-    div rcx
-    dec rdi
-    add dl, '0'
-    mov [rdi], dl
-    inc rbx
-    test rax, rax
-    jnz .convert
-    mov rax, 1
-    mov rsi, rdi
-    mov rdi, 1
-    mov rdx, rbx
-    syscall
-    add rsp, 32
-    ret
-
-; ========== ТОЧКА ВХОДА ==========
+; ============================
+; Чтение и парсинг ввода
+; ============================
 _start:
-    ; Читаем ввод
+    ; Чтение данных с ввода
     mov rax, 0
     mov rdi, 0
     lea rsi, [buf]
     mov rdx, 255
     syscall
 
-    ; Парсим N и M
+    ; Парсинг чисел (количество строк и столбцов)
     lea rsi, [buf]
     call skip_spaces
     call parse_int
@@ -104,25 +39,26 @@ _start:
     call parse_int
     mov [m], rax
 
-    ; Инициализация
-    xor rax, rax
+; ============================
+; Инициализация позиции
+; ============================
+    mov rax, 1
     mov [x], rax
     mov [y], rax
-    mov [minx], rax
-    mov [maxx], rax
-    mov [miny], rax
-    mov [maxy], rax
 
     call skip_spaces
 
-; ========== ОБРАБОТКА КОМАНД ==========
-.loop:
+; ============================
+; Обработка команд движения
+; ============================
+.check:
     mov al, [rsi]
     cmp al, 0
-    je .check_bounds
+    je .valid
     cmp al, 10
-    je .check_bounds
+    je .valid
 
+    ; Обработка команд 'L', 'R', 'U', 'D'
     cmp al, 'L'
     je .left
     cmp al, 'R'
@@ -131,85 +67,165 @@ _start:
     je .up
     cmp al, 'D'
     je .down
+
     jmp .invalid
 
+; ============================
+; Движение влево
+; ============================
 .left:
-    dec qword [y]
-    jmp .update
-.right:
-    inc qword [y]
-    jmp .update
-.up:
-    dec qword [x]
-    jmp .update
-.down:
-    inc qword [x]
-    jmp .update
-
-.update:
-    ; Обновление мин/макс
-    mov rax, [x]
-    mov rbx, [minx]
-    cmp rax, rbx
-    jge .no_minx
-    mov [minx], rax
-.no_minx:
-    cmp rax, [maxx]
-    jle .no_maxx
-    mov [maxx], rax
-.no_maxx:
-
     mov rax, [y]
-    cmp rax, [miny]
-    jge .no_miny
-    mov [miny], rax
-.no_miny:
-    cmp rax, [maxy]
-    jle .no_maxy
-    mov [maxy], rax
-.no_maxy:
+    dec rax
+    cmp rax, 1
+    jl .invalid
+    mov [y], rax
+    jmp .next
 
-    inc rsi
-    jmp .loop
-
-; ========== ПРОВЕРКА ГРАНИЦ ==========
-.check_bounds:
-    mov rax, [maxx]
-    sub rax, [minx]
-    inc rax         ; Длина по X
-    cmp rax, [n]
-    ja .invalid
-
-    mov rax, [maxy]
-    sub rax, [miny]
-    inc rax         ; Длина по Y
+; ============================
+; Движение вправо
+; ============================
+.right:
+    mov rax, [y]
+    inc rax
     cmp rax, [m]
-    ja .invalid
+    jg .invalid
+    mov [y], rax
+    jmp .next
 
-    ; Вывод Yes: (1 - minx, 1 - miny)
+; ============================
+; Движение вверх
+; ============================
+.up:
+    mov rax, [x]
+    dec rax
+    cmp rax, 1
+    jl .invalid
+    mov [x], rax
+    jmp .next
+
+; ============================
+; Движение вниз
+; ============================
+.down:
+    mov rax, [x]
+    inc rax
+    cmp rax, [n]
+    jg .invalid
+    mov [x], rax
+    jmp .next
+
+; ============================
+; Переход к следующей команде
+; ============================
+.next:
+    inc rsi
+    jmp .check
+
+; ============================
+; Успешный результат
+; ============================
+.valid:
     lea rsi, [msg_yes]
     call print_string
-
-    mov rax, 1
-    sub rax, [minx]
+    mov rax, [x]
     call print_number
-
     lea rsi, [comma]
     call print_string
-
-    mov rax, 1
-    sub rax, [miny]
+    mov rax, [y]
     call print_number
-
     lea rsi, [close_br]
     call print_string
     jmp .exit
 
+; ============================
+; Ошибка ввода
+; ============================
 .invalid:
     lea rsi, [msg_no]
     call print_string
 
+; ============================
+; Завершение программы
+; ============================
 .exit:
     mov rax, 60
     xor rdi, rdi
     syscall
+
+; ============================
+; Преобразование строки в число
+; ============================
+parse_int:
+    xor rax, rax
+    xor rbx, rbx
+    mov rcx, 10
+.parse_loop:
+    mov bl, [rsi]
+    cmp bl, '0'
+    jb .done
+    cmp bl, '9'
+    ja .done
+    sub bl, '0'
+    imul rax, rcx
+    add rax, rbx
+    inc rsi
+    jmp .parse_loop
+.done:
+    ret
+
+; ============================
+; Пропуск пробелов и табуляций
+; ============================
+skip_spaces:
+.skip_loop:
+    mov al, [rsi]
+    cmp al, ' '
+    je .skip
+    cmp al, 9
+    je .skip
+    ret
+.skip:
+    inc rsi
+    jmp .skip_loop
+
+; ============================
+; Вывод строки на экран
+; ============================
+print_string:
+    mov rdx, 0
+.strlen:
+    cmp byte [rsi+rdx], 0
+    je .print
+    inc rdx
+    jmp .strlen
+.print:
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+; ============================
+; Вывод числа на экран
+; ============================
+print_number:
+    mov rcx, 10
+    sub rsp, 32
+    mov rdi, rsp
+    add rdi, 32
+    mov rbx, 0
+.rev_loop:
+    xor rdx, rdx
+    div rcx
+    dec rdi
+    add dl, '0'
+    mov [rdi], dl
+    inc rbx
+    test rax, rax
+    jnz .rev_loop
+    mov rax, 1
+    mov rsi, rdi
+    mov rdi, 1
+    mov rdx, rbx
+    syscall
+    add rsp, 32
+    ret
